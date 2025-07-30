99cdf1b73c03e4970fba94e0e14238bc92742e3c
minematsu toshiya <mine.webstaff@gmail.com>
2025-07-30 13:00:34 +0900

    修正：Debug のため、 main.py を変更
    - 主に、ライブラリの読み込み問題を解決するために cwd の位置等のチェック

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import os
+
+
 def main():
     print("Hello from memory-mcp!")
+    print(f"Curent Path:{os.getcwd()}")
 
 
 if __name__ == "__main__":
     main()
+    file_dir = os.path.dirname(os.path.abspath(__file__)) 
+    print(f"filedir={file_dir}")
+    os.chdir(file_dir)
+    print(f"Current Path: {os.getcwd()}")
+
+    from mcp.server.fastmcp import FastMCP
